ccc450461139d00172cdeb78b231d5377afb3a64
Spring <silk.spring@yahoo.com>
2012-10-24 17:01:55 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RisingSun::Application.config.secret_token = 'f8c16f1085560df5760477d6b0d8eff83a24803ed8f4caad0dc3209ab8a33727a74d4559cff48683bf1890e9d81ae8d133d2a32e03e960500ed881b3022743b2'
